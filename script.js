d717e778c033db17996588b4bb10a84e4f588399
RayappaManashetti <166776697+RayappaManashetti@users.noreply.github.com>
2025-03-30 23:31:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Hello");
+
+// let btn = document.createElement("button");
+// btn.innerText="Click me!";
+// document.getElementById("mainContainer").before(btn);
+
+let ad = document.getElementById("arduino-uno");
+ad.onclick = () =>{
+    console.log("Hello world");
+}
